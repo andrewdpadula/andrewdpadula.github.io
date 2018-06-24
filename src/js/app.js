b3a5a6864294a61aed8155ac9f84aea73bbc6f68
Andrew Padula <andrewdpadula@gmail.com>
2018-06-24 17:01:11 -0400

    Updated layout, images

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,5 @@
-var navUlList = document.querySelectorAll('nav > ul > li');
-navUlList.forEach(function(elem){
-	elem.addEventListener('mouseover', mouseOver);
-	elem.addEventListener('mouseout', mouseOut);
-})
-function mouseOver(){
-	this.style.fontSize = '24px';
-}
-function mouseOut(){
-	this.style.fontSize = '16px';
-}
-
-
-var footerLink = document.querySelectorAll('footer > a');
-footerLink.forEach(function(elem){
+var hyperlinks = document.querySelectorAll('a');
+hyperlinks.forEach(function(elem){
 	elem.addEventListener('mouseover', mouseOver);
 	elem.addEventListener('mouseout', mouseOut);
 })
